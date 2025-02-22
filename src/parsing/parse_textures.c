a122900cef56721349377c4cd2c2a8564c4ff270
twillian <twillian.sc21@gmail.com>
2025-02-23 02:34:32 +0400

    norms: parse_textures.c function parse_textures splited adding all_elements_parsed, init_structure functions

--- src/parsing/parse_textures.c
+++ src/parsing/parse_textures.c
@@ -89,20 +89,45 @@ int	search_elements(char *line, t_game_data *initData, int pe)
 	return (0);
 }
 
-bool	parse_textures(int fd, t_game_data *initData, char **map_line)
+void	init_structure(t_game_data *initData)
 {
-	char		*line;
-	int			parsed_elements;
-	int			minimum_elements;
-	t_parsed_lines	*head;
-
-	minimum_elements = 6;
-	parsed_elements = 0;
 	initData->initlines = (t_parsed_lines**)malloc(sizeof(t_parsed_lines*));
 	*(initData->initlines) = (t_parsed_lines*)malloc(sizeof(t_parsed_lines));
 	(*initData->initlines)->next = NULL;
 	(*initData->initlines)->line = NULL;
 	(*initData->initlines)->id = 1;
+}
+
+bool	all_elements_parsed(int pe, t_parsed_lines	*h, char *l, char **map_l)
+{
+	if (pe < 6 || ft_lstsize_pl(h) < 6)
+	{
+		print_error("Parsing intit info", "Do not have 6 lines");
+		if (l)
+			free(l);
+		*map_l = NULL;
+		return (false);
+	}
+	if (l)
+	{
+		*map_l = ft_strdup(l);
+		free(l);
+	}
+	else
+		*map_l = NULL;
+	return (true);
+}
+
+bool	parse_textures(int fd, t_game_data *initData, char **map_line)
+{
+	char	*line;
+	int		parsed_elements;
+	int		minimum_elements;
+	t_parsed_lines	*head;
+
+	minimum_elements = 6;
+	parsed_elements = 0;
+	init_structure(initData);
 	head = *initData->initlines;
 	line = skip_empty_lines(fd);
 	while (line && !is_eof(*line) && parsed_elements < minimum_elements)
@@ -113,18 +138,7 @@ bool	parse_textures(int fd, t_game_data *initData, char **map_line)
 		line = skip_empty_lines(fd);
 		*(initData->initlines) = head;
 	}
-	if (parsed_elements < minimum_elements || ft_lstsize_pl(head) < 6)
-	{
-		print_error("Parsing intit info", "Do not have 6 lines");
-		if (line)
-			free(line);
-		*map_line = NULL;
+	if (!all_elements_parsed(parsed_elements, head, line, map_line))
 		return (exit_textures(NULL));
-	}
-	if (line)
-	{
-		*map_line = ft_strdup(line);
-		free(line);
-	}
 	return (true);
 }
